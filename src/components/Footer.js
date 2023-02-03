df4dae3ad84aac6c86a6b65f235dd1a43cee489e
Kirsten <kirstenmhattori@gmail.com>
2023-02-02 19:15:57 -0700

    made footer component with required juno college tag and removed out of box react files and code

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+  console.log('footer has mounted');
+  return(
+    <footer>
+      <p>Created at <a href="https://junocollege.com/">Juno College</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
